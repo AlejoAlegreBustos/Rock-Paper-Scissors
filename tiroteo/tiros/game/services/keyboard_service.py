4b92f7798199151ef6b361476d4302587ac0ed64
AlejoAlegreBustos <ale21020@byui.edu>
2022-10-22 13:37:23 -0300

    created repo tiros and loaded tiroteo game

---
+++ tiroteo/tiros/game/services/keyboard_service.py
@@ -0,0 +1,69 @@
+import pyray
+from game.shared.point import Point
+
+
+class KeyboardService:
+    """Detects player input. 
+    
+    The responsibility of a KeyboardService is to detect player key presses and translate them into 
+    a point representing a direction.
+
+    Attributes:
+        cell_size (int): For scaling directional input to a grid.
+    """
+
+    def __init__(self, cell_size = 1):
+        """Constructs a new KeyboardService using the specified cell size.
+        
+        Args:
+            cell_size (int): The size of a cell in the display grid.
+        """
+        self._cell_size = cell_size
+
+    def get_direction(self, player):
+        """Gets the selected direction based on the currently pressed keys.
+
+        Returns:
+            Point: The selected direction.
+        """
+        dx = 0
+        dy = 0
+
+        if player == 'one':
+            if pyray.is_key_down(pyray.KEY_A):
+                dx = -1
+            
+            if pyray.is_key_down(pyray.KEY_D):
+                dx = 1
+            
+            if pyray.is_key_down(pyray.KEY_W):
+                dy = -1
+            
+            if pyray.is_key_down(pyray.KEY_S):
+                dy = 1
+        elif player == 'two':
+            if pyray.is_key_down(pyray.KEY_J):
+                dx = -1
+        
+            if pyray.is_key_down(pyray.KEY_L):
+                dx = 1
+            
+            if pyray.is_key_down(pyray.KEY_I):
+                dy = -1
+            
+            if pyray.is_key_down(pyray.KEY_K):
+                dy = 1
+
+        direction = Point(dx, dy)
+        direction = direction.scale(self._cell_size)
+        
+        return direction
+
+    def is_shooting(self, player):
+        if player == 'one':
+            if pyray.is_key_down(pyray.KEY_X):
+                return True
+
+        elif player == 'two':
+            if pyray.is_key_down(pyray.KEY_M):
+                return True
